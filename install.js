3b3acc67adf27670052553f48873ec022576dd1b
6Morpheus6 <wanton-decay@gmx.net>
2024-09-10 22:22:45 +0200

    Add torch.js and remove torch cuda wheel for cross platform support

--- install.js
+++ install.js
@@ -26,7 +26,6 @@ module.exports = {
           "pip install --upgrade setuptools",
           "pip install -r requirements.txt",
           "pip uninstall torch torchvision torchaudio -y",
-          "pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121",
         ],
       },
     },
