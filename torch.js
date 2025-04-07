29c0009f5be9b453a9e93f31f4bfe1fcbfc8ad12
6Morpheus6 <wanton-decay@gmx.net>
2025-04-07 23:00:22 +0200

    50 series handling

--- torch.js
+++ torch.js
@@ -1,5 +1,17 @@
 module.exports = {
   run: [
+    // nvidia 50 series
+    {
+      "when": "{{gpu === 'nvidia' && kernel.gpus && kernel.gpus.find(x => / 50.+/.test(x.model))}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+      },
+      "next": null
+    },
+    // windows nvidia
     {
       "when": "{{platform === 'win32' && gpu === 'nvidia'}}",
       "method": "shell.run",
