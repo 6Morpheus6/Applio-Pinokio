75f98bdbdb1b0acbd32c79562af4d956b2d20ea8
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-12-19 04:58:50 +0100

    update

--- install.js
+++ install.js
@@ -1,10 +1,21 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   run: [
     {
       method: "shell.run",
       params: {
         message: "git clone https://github.com/IAHispano/Applio app",
       },
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "conda update -y -c conda-forge huggingface_hub",
+        ]
+      }
     },
     {
       method: "script.start",
@@ -19,8 +30,8 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
           "uv pip install -r ../requirements.txt",
         ]
